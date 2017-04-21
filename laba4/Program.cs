e8387d0b7ca84090928b197139e41d23ca188d87
Mike <mikedark@yandex.ru>
2017-04-21 20:28:15 +0300

    Добавлен нормальный вывод

--- laba4/Program.cs
+++ laba4/Program.cs
@@ -57,18 +57,43 @@ namespace laba4
             }
             return W;
         }
+        static void exit (int K, int[] A, string h)
+        {
+            Console.Write("{0}    = ",h);
+            int count = 0;
+            for (int i = 0; i < K; i++)
+            {
+                if (count == K / 5)
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.WriteLine();
+                    Console.Write("       ");
+                    count = 0;
+                }
+                if (A[i] == 1)
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                }
+                else
+                    Console.BackgroundColor = ConsoleColor.Black;
+
+                string str = String.Format("{0,3}", A[i]);
+                Console.Write(str);
+                count++;
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+            count = 0;
+            Console.WriteLine();
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
             int K = 15;
-            int[] A = {1,1,1,1,1,1,-1,1,-1,-1,1,1,1,1,1};
-            int[] fAke= { 1, -1, 1, 1, 1, 1, 1, 1, -1, -1, 1, 1, 1, 1, 1 };
-            int[] I = {1,-1,-1,-1,1,1,1,1,1,1,1,-1,-1,-1, 1};
-            int[] F = {1,1,1,1,1,1,-1,1,-1,-1,1,-1,-1,-1,-1};
+            int[] A = { 1, 1, 1, 1, -1, 1, 1, 1, 1, 1, -1, 1, 1, -1, 1 };
+            int[] fAke= { 1, -1, 1, -1, 1, -1, -1, 1, -1, -1, 1, -1, -1, -1, -1 };
+            int[] I = { 1, 1, 1, -1, 1, -1, -1, 1, -1, -1, 1, -1, 1, 1, 1 };
+            int[] F = { 1, 1, 1, 1, -1, -1, 1, 1, -1, 1, -1, -1, 1, -1, -1 };
             int[] Y = new int[K];
-          /* for (int i = 0; i < K; i++)
-            {
-                Y[i] = A[i];
-            }*/
             int[,] W = new int[K, K];
             W = wMas(W,K,A);
             W = wMas(W, K, I);
@@ -77,31 +102,21 @@ namespace laba4
             {
                 for (int j = 0; j < K; j++)
                 {
-                    Console.Write(W[j,k]+" ");
+                    string str = String.Format("{0,2} ", W[j,k]); 
+                    Console.Write(str);
                 }
                 Console.WriteLine();
             }
-            Console.Write("A    = ");
-            for (int i = 0; i < K; i++)
-            {
-                Console.Write(A[i]+" ");
-            }
-            Console.WriteLine();
-            Console.Write("fAke = ");
-            for (int i = 0; i < K; i++)
-            {
-                Console.Write(fAke[i] + " ");
-            }
+            exit(K, A, "A");
+            exit(K, I, "I");
+            exit(K, F, "F");
+            exit(K, fAke, "f");
             for (int k = 0; k < K; k++)
             {
                 Y[k] = fnet(W, fAke, k, K);
             }
-            Console.WriteLine();
-            Console.Write("new  = ");
-            for (int i = 0; i < K; i++)
-            {
-                Console.Write(Y[i] + " ");
-            }
+            exit(K, Y, "n");
+            Console.ReadKey();
         }
     }
 }
